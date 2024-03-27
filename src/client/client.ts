4606adda8bf42a28d39f4ca899f16cf87f74a2a1
Edward Timmer <edtimmer@gmail.com>
2024-03-27 00:58:58 -0400

    added stats and gui

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,7 +1,11 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import Stats from 'three/examples/jsm/libs/stats.module'
+import { GUI } from 'dat.gui'
 
 const scene = new THREE.Scene()
+scene.add(new THREE.AxesHelper(5))
+
 const camera = new THREE.PerspectiveCamera(
     75,
     window.innerWidth / window.innerHeight,
@@ -15,16 +19,21 @@ const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
-new OrbitControls(camera, renderer.domElement)
+// new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement)
+// controls.addEventListener('change', render) // use if there is no animation loop
 
 const geometry = new THREE.BoxGeometry()
+const torusGeometry = new THREE.TorusKnotGeometry( 16, 6, 200, 32 );
 const material = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
+    color: 0x00ff00,
     wireframe: true,
 })
 
 const cube = new THREE.Mesh(geometry, material)
+const torus = new THREE.Mesh(torusGeometry, material)
 scene.add(cube)
+// scene.add(torus)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
@@ -34,11 +43,49 @@ function onWindowResize() {
     render()
 }
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
+const gui = new GUI()
+
+const cubeFolder = gui.addFolder('Cube')
+cubeFolder.open()
+cubeFolder.add(cube, 'visible')
+
+const cubeRotationFolder = cubeFolder.addFolder('Rotation')
+cubeRotationFolder.add(cube.rotation, 'x', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'y', 0, Math.PI * 2)
+cubeRotationFolder.add(cube.rotation, 'z', 0, Math.PI * 2)
+cubeRotationFolder.open()
+
+const cubePositionFolder = cubeFolder.addFolder('Position')
+cubePositionFolder.add(cube.position, 'x', -10, 10)
+cubePositionFolder.add(cube.position, 'y', -10, 10)
+cubePositionFolder.add(cube.position, 'z', -10, 10)
+cubePositionFolder.open()
+
+const cubeScaleFolder = cubeFolder.addFolder('Scale')
+cubeScaleFolder.add(cube.scale, 'x', 0, 5)
+cubeScaleFolder.add(cube.scale, 'y', 0, 5)
+cubeScaleFolder.add(cube.scale, 'z', 0, 5)
+cubeScaleFolder.open()
+
+// const torusFolder = gui.addFolder('Torus')
+// torusFolder.add(torus.rotation, 'x', 0, Math.PI * 2)
+// torusFolder.add(torus.rotation, 'y', 0, Math.PI * 2)
+// torusFolder.add(torus.rotation, 'z', 0, Math.PI * 2)
+// torusFolder.open()
+
+// const cameraFolder = gui.addFolder('Camera')
+// cameraFolder.add(camera.position, 'z', 0, 20)
+// cameraFolder.open()
+
 function animate() {
     requestAnimationFrame(animate)
-    cube.rotation.x += 0.01
-    cube.rotation.y += 0.01
+    // cube.rotation.x += 0.01
+    // cube.rotation.y += 0.01
     render()
+    stats.update()
 }
 
 function render() {
@@ -46,3 +93,4 @@ function render() {
 }
 
 animate()
+// render()
